4a0ba67708168f949893dcaf684d6a713f88ede2
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2024-09-28 22:45:45 +0700

    Jobsheet 4 tugas array 1

--- dasarWeb/Pertemuan 4/tugasarray1.php
+++ dasarWeb/Pertemuan 4/tugasarray1.php
@@ -3,11 +3,17 @@ $nilaiSiswa = [85,92,78,64,90,75,88,79,70,96];
 
 sort($nilaiSiswa);
 $duaTerendah = array_slice($nilaiSiswa, 0, 2);
-$duaTertinggi = array_slice($nilaiSiswa, -1, 2);
+$duaTertinggi = array_slice($nilaiSiswa, -2, 2);
 
 $exclude = array_merge($duaTerendah, $duaTertinggi);
 
 $totalFiltered = array_sum($nilaiSiswa) - array_sum($exclude);
 
-echo "Nilai terfilter: $totalFiltered";
+echo "2 nilai tertinggi: " . implode(", ", $duaTertinggi) . "<br>";
+echo "2 nilai terendah: " . implode(", ", $duaTerendah) . "<br>";
+echo "Nilai terfilter: $totalFiltered <br>";
+
+$rataRata = $totalFiltered / (count($nilaiSiswa) - 4);
+
+echo "Rata-rata nilai terfilter: $rataRata";
 ?>
