7ad5caca9689ac603b57a2548c28e570b33162c4
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2024-10-04 08:41:54 +0700

    jobsheet 6 praktikum bagian 1

---
+++ dasarWeb/Pertemuan 6/array_1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <h2>Array Terindeks</h2>
+        <?php
+        $Listdosen = ["Elok Nur Hanmdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+        echo $Listdosen[2] . "<br>";
+        echo $Listdosen[0] . "<br>";
+        echo $Listdosen[1] . "<br>";
+        ?>
+    </body>
+</html>
