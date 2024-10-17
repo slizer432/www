a7a6c3236d4e2990473c936bfd8bc280d1ec5daa
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2024-10-17 11:15:23 +0700

    fix jobsheet 7 Praktikum 7

--- dasarWeb/Pertemuan 7/validasi_script.php
+++ dasarWeb/Pertemuan 7/validasi_script.php
@@ -2,6 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nama = $_POST['nama'];
     $email = $_POST['email'];
+    $password = $_POST['password'];
     $errors = array();
 
     // Validasi Nama
